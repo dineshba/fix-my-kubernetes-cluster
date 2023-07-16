58586a0340c8c40162c772bd3c211c7c7ba0fb44
Dinesh B <dineshudt17@gmail.com>
2023-07-16 21:00:34 +0530

    Add issue2
    Signed-off-by: Dinesh B <dineshudt17@gmail.com>

---
+++ 2/create-cluster-with-issue.sh
@@ -0,0 +1,25 @@
+clusterName="issue2"
+kind create cluster --name $clusterName
+printf "Waiting for nodes to be ready\n"
+kubectl wait --for=condition=Ready nodes --all --timeout=60s
+
+# wait for default sa to be created
+kubectl wait --for=jsonpath='{.status.phase}'=Active ns/default --timeout=60s
+
+defaultServiceAccountPresent=$(kubectl get sa -n default | grep default | wc -l)
+while [ $defaultServiceAccountPresent -eq 0 ]; do
+  printf "Waiting for default SA to be created\n"
+  sleep 5
+  defaultServiceAccountPresent=$(kubectl get sa -n default | grep default | wc -l)
+done
+
+# create an issue
+docker exec -it $(docker ps | grep $clusterName | awk '{ print $1}') systemctl stop kubelet
+
+
+printf "\n"
+printf "Applying Pods\n"
+kubectl apply -f pod1.yaml
+printf "\n\nCan you help in making the pending pod run?\n"
+kubectl get pods -n default
+printf "\n\n"
